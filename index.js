973664aa13a9e43e52ab6f8cebe16fbc987cdcf7
Juan Ovalle <jpovalleg@gmail.com>
2024-04-07 02:47:54 -0500

    usando variable de entorno PORT

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
-app.listen(3000);
-console.log('Server on port', 3000);
+app.listen(port);
+console.log('Server on port', port);
