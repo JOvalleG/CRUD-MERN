07c09d3e5a27df7b6793bc1040b008404cb130cd
CamiloArdila <anardilad@unal.edu.co>
2024-04-08 16:32:31 -0500

    Creacion de rutas 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,25 @@
+import React from "react";
 import {Routes, Route} from "react-router-dom";
-import './App.css'
+import "./App.css"
+import Home from "./pages/Home";
+import Salud from "./pages/salud";
+import Vivienda from "./pages/vivienda";
+import Personas from "./pages/personas";
+
 
 function App() {
 
   return (
-    <div>
+    <>
       <Routes>
-        <Route path="/" />
-        <Route path="/personas" />
+        <Route path="/" element={<Home />} />
+        <Route path="/salud" element={<Salud />} />
+        <Route path="/vivienda" element={<Vivienda />} />
+        <Route path="/personas" element={<Personas />} />
       </Routes>
-    </div>
+    </>
+
+
   )
 }
 
