f4789887feb4d9b5ef07f0af9cc86babe94252ff
Juan Ovalle <jpovalleg@gmail.com>
2024-04-08 16:26:40 -0500

    Estructura Router y Navbar

---
+++ frontend/src/pages/Familia.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export function Familias() {
+    return (
+        <div className="container my-5">
+            <h2 className='text-center mb-3'>Familias</h2>
+        </div>
+    )
+}
+
+function ListaFamilias() {
+    return (
+        <>
+        <h2 className='text-center mb-3'>Lista de familias</h2>
+        </>
+    )
+}
